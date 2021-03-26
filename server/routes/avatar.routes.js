393c780a2279fd46cf89ac93f630c010963acc59
Lau <dcalau28@gmail.com>
2021-03-26 22:51:24 +0700

    Update avatar.routes.js

--- server/routes/avatar.routes.js
+++ server/routes/avatar.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const router = express.Router();
 
-const controller = require("../controllers/uploadSingleImg.controller");
+// const controller = require("../controllers/uploadSingleImg.controller");
 const dbConfig = require("../config/db.config");
 
 const url = dbConfig.URI;
