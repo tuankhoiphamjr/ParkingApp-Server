6aff6898133e9f1ba0748c84189f157e915ed320
lucius99 <47803507+lucius99@users.noreply.github.com>
2021-03-28 18:07:00 +0700

    refactor for avatar API

--- server/routes/avatar.routes.js
+++ server/routes/avatar.routes.js
@@ -2,69 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const router = express.Router();
 
-const controller = require("../controllers/uploadSingleImg.controller");
-const dbConfig = require("../config/db.config");
-const url = dbConfig.URI;
-
-const connect = mongoose.createConnection(url, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-let gfs;
-
-connect.once("open", async () => {
-  // initialize stream
-  gfs = await new mongoose.mongo.GridFSBucket(connect.db, {
-    bucketName: "avatar",
-  });
-});
+const controller = require("../controllers/image.controller");
 
 // Route for AVATAR
 
-router.get("/file/:id", (req, res, next) => {
-  gfs
-    .find({ _id: mongoose.Types.ObjectId(req.params.id) })
-    .toArray((err, files) => {
-      if (!files[0] || files.length === 0) {
-        return res.status(200).json({
-          success: false,
-          message: "No files available",
-        });
-      }
+router.get("/file/:id", controller.getImageInfoController);
 
-      res.status(200).json({
-        success: true,
-        file: files[0],
-      });
-    });
-});
-
-router.get("/:id", (req, res, next) => {
-  gfs
-    .find({ _id: mongoose.Types.ObjectId(req.params.id) })
-    .toArray((err, files) => {
-      if (!files[0] || files.length === 0) {
-        return res.status(200).json({
-          success: false,
-          message: "No files available",
-        });
-      }
-
-      if (
-        files[0].contentType === "image/jpeg" ||
-        files[0].contentType === "image/png" ||
-        files[0].contentType === "image/svg+xml"
-      ) {
-        // render image to browser
-        gfs.openDownloadStreamByName(files[0].filename).pipe(res);
-      } else {
-        res.status(404).json({
-          err: "Not an image",
-        });
-      }
-    });
-});
+router.get("/:id", controller.showImage);
 
 router.post("/upload", controller.uploadFile);
 
