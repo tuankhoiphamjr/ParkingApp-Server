cb86d37c0c69eebaea59d54d499077be7a09c87f
Pham Khoi <khoi.phambkit_5ting@hcmut.edu.vn>
2021-04-09 19:27:41 +0700

    set config

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -3,10 +3,10 @@ let PORT = 8080;
 let DB = "TestDB";
 let PASSWORD = "lauluckhoi123";
 let HOSTNAME = "lau";
-let defaultAvatarId = "60603af0973a6642ac39b6f2"
+let defaultAvatarId = "60603af0973a6642ac39b6f2";
 
 module.exports = {
-  defaultAvatarId,
-  // URI: `mongodb+srv://lau:lauluckhoi123@cluster0.uypbs.mongodb.net/TestDB?retryWrites=true&w=majority`,
-  URI: "mongodb://localhost:27017/bezkoder_files_db"
+      defaultAvatarId,
+      URI: `mongodb+srv://lau:lauluckhoi123@cluster0.uypbs.mongodb.net/TestDB?retryWrites=true&w=majority`,
+      // URI: "mongodb://localhost:27017/bezkoder_files_db"
 };
