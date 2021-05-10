8dd00e377196d7bb640565754cbd69630ab6c52d
tuankhoiphamjr <khoi.phambkit_5ting@hcmut.edu.vn>
2021-05-10 10:11:10 +0700

    add field isActive vehicle, edit api related to this field and delete vehicle api

--- server/routes/vehicle.routes.js
+++ server/routes/vehicle.routes.js
@@ -6,6 +6,9 @@ const controller = require("../controllers/vehicle.controller");
 router.get("/", controller.getVehicleInfoController);
 
 router.post("/", controller.addVehicleController);
+
 router.post("/edit", controller.updateVehicleInfo);
+
+router.post("/delete", controller.deleteVehicleInfo);
 
 module.exports = router;
