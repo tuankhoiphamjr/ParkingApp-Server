942368d71c542c82e71cefb1109a9efd88c32efc
tuankhoiphamjr <khoi.phambkit_5ting@hcmut.edu.vn>
2021-06-02 12:54:28 +0700

    count api for admin

---
+++ server/controllers/adminParking.controller.js
@@ -0,0 +1,101 @@
+const parkingServices = require("../services/parking.service");
+const monitorService = require("../services/monitorParking.service");
+const userServices = require("../services/user.service");
+const feedbackService = require("../services/feedback.service");
+
+// Get all parking has field isVerified equal to false info
+exports.getParkingsNeedVerified = async (req, res) => {
+      let result = await parkingServices.getCensorshipParking();
+      if (!result.status) {
+            return res
+                  .status(400)
+                  .json({ status: false, message: result.message });
+      }
+      return res.status(200).json({ status: true, result: result.result });
+};
+
+// Verify a Parking By Admin
+exports.verifyParkingController = async (req, res) => {
+      let parkingId = req.params.parkingId;
+      let ownerId = req.body.ownerId;
+      let result = await parkingServices.verifyParking(parkingId, true);
+      if (!result) {
+            return res
+                  .status(400)
+                  .json({ status: false, message: "Some thing wrong" });
+      }
+      if (!result?.status) {
+            return res
+                  .status(400)
+                  .json({ status: false, message: result.message });
+      }
+      let response = await monitorService.createNewMonitor(ownerId, parkingId);
+      if (!response) {
+            return res
+                  .status(400)
+                  .json({ status: false, message: "Some thing wrong" });
+      }
+      if (!response?.status) {
+            res.status(400).json({ message: response.message });
+            return;
+      }
+      return res.status(200).json({ status: true, result: result.message });
+};
+
+// Get number of user and owner
+exports.getNumOfUserAndOwner = async (req, res) => {
+      let result = await userServices.getNumOfUserAndOwner();
+      if (!result) {
+            return res
+                  .status(400)
+                  .json({ status: false, message: "Some thing wrong" });
+      }
+      if (!result.status) {
+            return res
+                  .status(400)
+                  .json({ status: false, message: result.message });
+      }
+      return res.status(200).json({
+            numOfUser: result.numberOfUser,
+            numOfOwner: result.numberOfOwner,
+            status: true,
+      });
+};
+
+// Get number of parking
+exports.getNumOfParking = async (req, res) => {
+      let result = await parkingServices.getAllVerifiedParkingInfo();
+      if (!result) {
+            return res
+                  .status(400)
+                  .json({ status: false, message: "Some thing wrong" });
+      }
+      if (!result.status) {
+            return res
+                  .status(400)
+                  .json({ status: false, message: result.message });
+      }
+      return res.status(200).json({
+            numOfParking: result.result.length,
+            status: true,
+      });
+};
+
+// Get number of evaluate
+exports.getNumOfEvaluate = async (req, res) => {
+      let result = await feedbackService.getNumberOfFeedback();
+      if (!result) {
+            return res
+                  .status(400)
+                  .json({ status: false, message: "Some thing wrong" });
+      }
+      if (!result.status) {
+            return res
+                  .status(400)
+                  .json({ status: false, message: result.message });
+      }
+      return res.status(200).json({
+            numOfEvaluate: result.result,
+            status: true,
+      });
+};
--- server/services/user.service.js
+++ server/services/user.service.js
@@ -6,132 +6,162 @@ const User = db.user;
 
 // Create User in DB
 createUser = async (
-  phoneNumber,
-  password,
-  role,
-  firstName,
-  lastName,
-  email, 
-  avatar
+      phoneNumber,
+      password,
+      role,
+      firstName,
+      lastName,
+      email,
+      avatar
 ) => {
-  let result = await User.create({
-    phoneNumber,
-    password,
-    role,
-    firstName,
-    lastName,
-    email,
-    avatar
-  });
-  return { result, status: true };
+      let result = await User.create({
+            phoneNumber,
+            password,
+            role,
+            firstName,
+            lastName,
+            email,
+            avatar,
+      });
+      return { result, status: true };
 };
 
 // Change status of user
 setUserStatus = async (userId, status) => {
-  let result;
-  await User.findOneAndUpdate(
-    { _id: mongoose.Types.ObjectId(userId) },
-    { isActive: status },
-    (err, data) => {
-      if (err) {
-        result = { message: err, status: false };
-      }
-      result = { message: "Success", status: true };
-    }
-  );
-  return result;
+      let result;
+      await User.findOneAndUpdate(
+            { _id: mongoose.Types.ObjectId(userId) },
+            { isActive: status },
+            (err, data) => {
+                  if (err) {
+                        result = { message: err, status: false };
+                  }
+                  result = { message: "Success", status: true };
+            }
+      );
+      return result;
 };
 
 // Get all user info by number phone
 getUserByPhoneNumberAndRole = async (phoneNumber, role) => {
-  let result = await User.findOne({ phoneNumber, role })
-    // .populate("avatar", "-_id -__v")
-    // .select("-__v");
-  if (!result) {
-    return { message: `User with role ${role} not found`, status: false };
-  }
-  return { result, status: true };
+      let result = await User.findOne({ phoneNumber, role });
+      if (!result) {
+            return {
+                  message: `User with role ${role} not found`,
+                  status: false,
+            };
+      }
+      return { result, status: true };
 };
 
 // Get all user info by _id
 getUserById = async (userID) => {
-  let result = await User.findOne({ _id: userID })
-  if (!result) {
-    return { message: "User not found", status: false };
-  }
-  return { result, status: true };
+      let result = await User.findOne({ _id: userID });
+      if (!result) {
+            return { message: "User not found", status: false };
+      }
+      return { result, status: true };
 };
 
 // Compare password
 comparePassword = async (password, passHashinDB) => {
-  let passwordIsValid = await bcrypt.compareSync(password, passHashinDB);
-  if (!passwordIsValid) {
-    return {
-      status: false,
-      message: "Invalid Password!",
-    };
-  }
-  return {
-    status: true,
-    message: "Valid Password!",
-  };
+      let passwordIsValid = await bcrypt.compareSync(password, passHashinDB);
+      if (!passwordIsValid) {
+            return {
+                  status: false,
+                  message: "Invalid Password!",
+            };
+      }
+      return {
+            status: true,
+            message: "Valid Password!",
+      };
 };
 
 // Update password of user in DB
 updatePassword = async (userId, newPassword) => {
-  let result;
-  await User.findOneAndUpdate(
-    { _id: mongoose.Types.ObjectId(userId) },
-    { password: bcrypt.hashSync(newPassword, 8) },
-    (err, data) => {
-      if (err) {
-        result = { message: err, status: false };
-      }
-      result = { message: "Success", status: true };
-    }
-  );
+      let result;
+      await User.findOneAndUpdate(
+            { _id: mongoose.Types.ObjectId(userId) },
+            { password: bcrypt.hashSync(newPassword, 8) },
+            (err, data) => {
+                  if (err) {
+                        result = { message: err, status: false };
+                  }
+                  result = { message: "Success", status: true };
+            }
+      );
 
-  return result;
+      return result;
 };
 
 // Update user info in DB
 updateUserInfo = async (userId, firstName, lastName, email, avatar) => {
-  let result;
-  await User.findOneAndUpdate(
-    { _id: mongoose.Types.ObjectId(userId) },
-    { firstName: firstName, lastName: lastName, email: email, avatar: avatar },
-    (err, data) => {
-      if (err) {
-        result = { message: err, status: false };
-      }
-      result = { message: "Success", status: true };
-    }
-  );
-  return result;
+      let result;
+      await User.findOneAndUpdate(
+            { _id: mongoose.Types.ObjectId(userId) },
+            {
+                  firstName: firstName,
+                  lastName: lastName,
+                  email: email,
+                  avatar: avatar,
+            },
+            (err, data) => {
+                  if (err) {
+                        result = { message: err, status: false };
+                  }
+                  result = { message: "Success", status: true };
+            }
+      );
+      return result;
 };
 
 // Checking if user existed with ${role}
 checkUserExist = async (phoneNumber, role) => {
-  let result = await User.findOne({ phoneNumber: phoneNumber, role: role });
-  if (result) {
-    return {
-      status: true,
-      message: `Failed!! User has been register with role ${role}`,
-    };
-  }
+      let result = await User.findOne({ phoneNumber: phoneNumber, role: role });
+      if (result) {
+            return {
+                  status: true,
+                  message: `Failed!! User has been register with role ${role}`,
+            };
+      }
 
-  return { status: false, message: "User not registed yet" };
+      return { status: false, message: "User not registed yet" };
+};
+
+// Get number of user and owner
+getNumOfUserAndOwner = async () => {
+      let result = await User.find({ role: "user" });
+      if (result.length === 0) {
+            return {
+                  message: "There are no User",
+                  status: false,
+            };
+      }
+      let res = await User.find({ role: "owner" });
+      if (res.length === 0) {
+            return {
+                  message: "There are no Owner",
+                  status: false,
+            };
+      }
+      return {
+            numberOfUser: result.length,
+            numberOfOwner: res.length,
+            status: true,
+      };
 };
 
 const userServices = {
-  setUserStatus,
-  createUser,
-  getUserByPhoneNumberAndRole,
-  getUserById,
-  comparePassword,
-  updatePassword,
-  updateUserInfo,
-  checkUserExist,
+      setUserStatus,
+      createUser,
+      getUserByPhoneNumberAndRole,
+      getUserById,
+      comparePassword,
+      updatePassword,
+      updateUserInfo,
+      checkUserExist,
+      getNumOfUserAndOwner,
 };
 
 module.exports = userServices;
